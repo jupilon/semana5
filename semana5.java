3d42c7779097fe698cddcc0fd972b8734ad70020
jupilon <julianapilon2016@gmail.com>
2021-09-02 17:55:35 +0000

    semana

--- semana5.java
+++ semana5.java
@@ -1,8 +1,8 @@
 public class semana5 {
     public static void main (String[]args){
        
-        long fat=1;
-        for(int=5;i>=1;i--){
+        long fat=1L;
+        for(int i=5;i>=1;i--){
             fat=i*fat;
 
         }
